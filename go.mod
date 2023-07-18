3f209f70c727f6f8a04f25886167f65a30390be5
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-07-19 00:11:52 +0530

    Add a package for check  domain functionality

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/moonman369/Go-Email-Verifier
+
+go 1.20
--- main.go
+++ main.go
@@ -4,9 +4,9 @@ import (
 	"bufio"
 	"fmt"
 	"log"
-	"net"
 	"os"
-	"strings"
+
+	emailverifier "github.com/moonman369/Go-Email-Verifier/email-verifier"
 )
 
 func main() {
@@ -14,7 +14,7 @@ func main() {
 	fmt.Printf("domain, hasMX, hasSPF, spfRecord, hasDMARC, dmarcRecord\n")
 
 	for scanner.Scan() {
-		checkDomain(scanner.Text())
+		emailverifier.CheckDomain(scanner.Text())
 	}
 
 	if err := scanner.Err(); err != nil {
@@ -22,49 +22,49 @@ func main() {
 	}
 }
 
-func checkDomain(domain string) {
-	var hasMX, hasSPF, hasDMARC bool
-	var spfRecord, dmarcRecord string
+// func checkDomain(domain string) {
+// 	var hasMX, hasSPF, hasDMARC bool
+// 	var spfRecord, dmarcRecord string
 
-	// Checking MX Records
-	mxRecords, err := net.LookupMX(domain)
-	if err != nil {
-		log.Printf("Error: %v\n", err)
-	}
-	if len(mxRecords) > 0 {
-		hasMX = true
-	}
+// 	// Checking MX Records
+// 	mxRecords, err := net.LookupMX(domain)
+// 	if err != nil {
+// 		log.Printf("Error: %v\n", err)
+// 	}
+// 	if len(mxRecords) > 0 {
+// 		hasMX = true
+// 	}
 
-	// Checking SPF Records
-	txtRecords, err := net.LookupTXT(domain)
-	if err != nil {
-		log.Printf("Error: %v\n", err)
-	}
-	for _, record := range txtRecords {
-		if strings.HasPrefix(record, "v=spf1") {
-			hasSPF = true
-			spfRecord = record
-			break
-		}
-	}
+// 	// Checking SPF Records
+// 	txtRecords, err := net.LookupTXT(domain)
+// 	if err != nil {
+// 		log.Printf("Error: %v\n", err)
+// 	}
+// 	for _, record := range txtRecords {
+// 		if strings.HasPrefix(record, "v=spf1") {
+// 			hasSPF = true
+// 			spfRecord = record
+// 			break
+// 		}
+// 	}
 
-	dmarcRecords, err := net.LookupTXT("_dmarc." + domain)
-	if err != nil {
-		log.Printf("Error: %v\n", err)
-	}
-	for _, record := range dmarcRecords {
-		if strings.HasPrefix(record, "v=DMARC1") {
-			hasDMARC = true
-			dmarcRecord = record
-			break
-		}
-	}
+// 	dmarcRecords, err := net.LookupTXT("_dmarc." + domain)
+// 	if err != nil {
+// 		log.Printf("Error: %v\n", err)
+// 	}
+// 	for _, record := range dmarcRecords {
+// 		if strings.HasPrefix(record, "v=DMARC1") {
+// 			hasDMARC = true
+// 			dmarcRecord = record
+// 			break
+// 		}
+// 	}
 
-	fmt.Printf("Domain:\t\t%v\n", domain)
-	fmt.Printf("Has MX:\t\t%v\n", hasMX)
-	fmt.Printf("Has SPF:\t%v\n", hasSPF)
-	fmt.Printf("SPF Record:\t%v\n", spfRecord)
-	fmt.Printf("Has DMARC:\t%v\n", hasDMARC)
-	fmt.Printf("DMARC Record:\t%v\n", dmarcRecord)
-	fmt.Println()
-}
+// 	fmt.Printf("Domain:\t\t%v\n", domain)
+// 	fmt.Printf("Has MX:\t\t%v\n", hasMX)
+// 	fmt.Printf("Has SPF:\t%v\n", hasSPF)
+// 	fmt.Printf("SPF Record:\t%v\n", spfRecord)
+// 	fmt.Printf("Has DMARC:\t%v\n", hasDMARC)
+// 	fmt.Printf("DMARC Record:\t%v\n", dmarcRecord)
+// 	fmt.Println()
+// }
