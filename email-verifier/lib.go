9eb8ce875447dd3ce48b27006936e653cddec497
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-07-19 00:46:42 +0530

    Create simple API Endpoint for verifying email domains: - Add gorilla/mux to dep list - Add `/verify` endpoint with POST method for sending domain path for verification

--- email-verifier/lib.go
+++ email-verifier/lib.go
@@ -7,9 +7,19 @@ import (
 	"strings"
 )
 
-func CheckDomain(domain string) {
-	var hasMX, hasSPF, hasDMARC bool
-	var spfRecord, dmarcRecord string
+type VerificationResult struct {
+	Domain      string `json:"domain"`
+	HasMX       bool   `json:"hasMX"`
+	HasSPF      bool   `json:"hasSPF"`
+	SPFRecord   string `json:"spfRecord"`
+	HasDMARC    bool   `json:"hasDMARC"`
+	DMARCRecord string `json:"dmarcRecord"`
+}
+
+func CheckDomain(domain string) VerificationResult {
+	var ver VerificationResult
+
+	ver.Domain = domain
 
 	// Checking MX Records
 	mxRecords, err := net.LookupMX(domain)
@@ -17,7 +27,7 @@ func CheckDomain(domain string) {
 		log.Printf("Error: %v\n", err)
 	}
 	if len(mxRecords) > 0 {
-		hasMX = true
+		ver.HasMX = true
 	}
 
 	// Checking SPF Records
@@ -27,8 +37,8 @@ func CheckDomain(domain string) {
 	}
 	for _, record := range txtRecords {
 		if strings.HasPrefix(record, "v=spf1") {
-			hasSPF = true
-			spfRecord = record
+			ver.HasSPF = true
+			ver.SPFRecord = record
 			break
 		}
 	}
@@ -39,17 +49,19 @@ func CheckDomain(domain string) {
 	}
 	for _, record := range dmarcRecords {
 		if strings.HasPrefix(record, "v=DMARC1") {
-			hasDMARC = true
-			dmarcRecord = record
+			ver.HasDMARC = true
+			ver.DMARCRecord = record
 			break
 		}
 	}
 
-	fmt.Printf("Domain:\t\t%v\n", domain)
-	fmt.Printf("Has MX:\t\t%v\n", hasMX)
-	fmt.Printf("Has SPF:\t%v\n", hasSPF)
-	fmt.Printf("SPF Record:\t%v\n", spfRecord)
-	fmt.Printf("Has DMARC:\t%v\n", hasDMARC)
-	fmt.Printf("DMARC Record:\t%v\n", dmarcRecord)
+	fmt.Printf("Domain:\t\t%v\n", ver.Domain)
+	fmt.Printf("Has MX:\t\t%v\n", ver.HasMX)
+	fmt.Printf("Has SPF:\t%v\n", ver.HasSPF)
+	fmt.Printf("SPF Record:\t%v\n", ver.SPFRecord)
+	fmt.Printf("Has DMARC:\t%v\n", ver.HasDMARC)
+	fmt.Printf("DMARC Record:\t%v\n", ver.DMARCRecord)
 	fmt.Println()
+
+	return ver
 }
--- main.go
+++ main.go
@@ -1,70 +1,54 @@
 package main
 
 import (
-	"bufio"
+	"encoding/json"
 	"fmt"
 	"log"
-	"os"
+	"net/http"
 
+	"github.com/gorilla/mux"
 	emailverifier "github.com/moonman369/Go-Email-Verifier/email-verifier"
 )
 
-func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Printf("domain, hasMX, hasSPF, spfRecord, hasDMARC, dmarcRecord\n")
-
-	for scanner.Scan() {
-		emailverifier.CheckDomain(scanner.Text())
-	}
-
-	if err := scanner.Err(); err != nil {
-		log.Fatal("Error: Invalid input: %v\n", err)
-	}
+type Domain struct {
+	Path string `json:"path"`
 }
 
-// func checkDomain(domain string) {
-// 	var hasMX, hasSPF, hasDMARC bool
-// 	var spfRecord, dmarcRecord string
-
-// 	// Checking MX Records
-// 	mxRecords, err := net.LookupMX(domain)
-// 	if err != nil {
-// 		log.Printf("Error: %v\n", err)
-// 	}
-// 	if len(mxRecords) > 0 {
-// 		hasMX = true
-// 	}
-
-// 	// Checking SPF Records
-// 	txtRecords, err := net.LookupTXT(domain)
-// 	if err != nil {
-// 		log.Printf("Error: %v\n", err)
-// 	}
-// 	for _, record := range txtRecords {
-// 		if strings.HasPrefix(record, "v=spf1") {
-// 			hasSPF = true
-// 			spfRecord = record
-// 			break
-// 		}
-// 	}
-
-// 	dmarcRecords, err := net.LookupTXT("_dmarc." + domain)
-// 	if err != nil {
-// 		log.Printf("Error: %v\n", err)
-// 	}
-// 	for _, record := range dmarcRecords {
-// 		if strings.HasPrefix(record, "v=DMARC1") {
-// 			hasDMARC = true
-// 			dmarcRecord = record
-// 			break
-// 		}
-// 	}
-
-// 	fmt.Printf("Domain:\t\t%v\n", domain)
-// 	fmt.Printf("Has MX:\t\t%v\n", hasMX)
-// 	fmt.Printf("Has SPF:\t%v\n", hasSPF)
-// 	fmt.Printf("SPF Record:\t%v\n", spfRecord)
-// 	fmt.Printf("Has DMARC:\t%v\n", hasDMARC)
-// 	fmt.Printf("DMARC Record:\t%v\n", dmarcRecord)
-// 	fmt.Println()
+// type VerificationResult struct {
+// 	Domain      *Domain `json:"domain"`
+// 	HasMX       bool    `json:"hasMX"`
+// 	HasSPF      bool    `json:"hasSPF"`
+// 	SPFRecord   string  `json:"spfRecord"`
+// 	HasDMARC    bool    `json:"hasDMARC"`
+// 	DMARCRecord string  `json:"dmarcRecord"`
 // }
+
+func verifyDomain(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var domain Domain
+	_ = json.NewDecoder(r.Body).Decode(&domain)
+	res := emailverifier.CheckDomain(domain.Path)
+	json.NewEncoder(w).Encode(res)
+}
+
+func main() {
+	// NATIVE APPLICATION CODE
+	// scanner := bufio.NewScanner(os.Stdin)
+	// fmt.Printf("domain, hasMX, hasSPF, spfRecord, hasDMARC, dmarcRecord\n")
+
+	// for scanner.Scan() {
+	// 	emailverifier.CheckDomain(scanner.Text())
+	// }
+
+	// if err := scanner.Err(); err != nil {
+	// 	log.Fatal("Error: Invalid input: %v\n", err)
+	// }
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/verify", verifyDomain).Methods("POST")
+	fmt.Println("Starting server at port 8080")
+	if err := http.ListenAndServe("0.0.0.0:8080", r); err != nil {
+		log.Fatal(err)
+	}
+}
